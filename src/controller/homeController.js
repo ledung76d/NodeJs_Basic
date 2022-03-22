1290f427d20d71dc296cdc8a4169815d2f44105c
Anh Dung <ledung76d@gmail.com>
2022-03-22 18:13:57 +0700

    t

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,4 @@
-import res from 'express/lib/response';
+import res, { json } from 'express/lib/response';
 import pool from '../configs/connectDB'
 
 let getHomepage = async (req, res) => {
@@ -19,8 +19,34 @@ let getDetailPage = async (req, res) => {
     console.log("check rows", user[0])
     return res.send(JSON.stringify(user[0]))
 }
+let createNewUser = async (req, res) => {
+    console.log("check:", req.body);
+    let { firstName, lastName, email, address } = req.body;
+    await pool.execute(`INSERT into users(firstName, lastName, email, address) values (?,?,?,?) `, [firstName, lastName, email, address])
+    return res.redirect('/')
+}
+let deleteUser = async (req, res) => {
+    let userId = req.body.userId;
+    await pool.execute(`DELETE FROM users WHERE id = ?`, [userId])
+    return res.redirect('/');
+}
+let getEditPage = async (req, res) => {
+    let id = req.params.id;
+    let [user] = await pool.execute('Select * from users where id =?', [id])
+    return res.render('update.ejs', { dataUser: user[0] })
+}
+let postUpdateUser = async (req, res) => {
+    console.log("check request: ", req.body)
+    let { firstName, lastName, email, address, id } = req.body;
+    // await pool.execute(`UPDATE users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?`, [firstName, lastName, email, address, id])
+    return res.send('Hello update user')
+}
 
 module.exports = {
     getHomepage,
-    getDetailPage
+    getDetailPage,
+    createNewUser,
+    deleteUser,
+    getEditPage,
+    postUpdateUser,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,10 +1,16 @@
 import express from "express";
+import { home } from "nodemon/lib/utils";
 import homeController from '../controller/homeController';
 let router = express.Router();
 
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
     router.get('/detail/user/:id', homeController.getDetailPage)
+
+    router.post('/create-new-user', homeController.createNewUser)
+    router.post('/delete-user', homeController.deleteUser)
+    router.get('/edit-user/:id', homeController.getEditPage)
+    router.post('/update-user', homeController.postUpdateUser)
     router.get('/about', (req, res) => {
         res.send(`I'm Eric!`)
     })
